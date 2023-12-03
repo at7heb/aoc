844e091255f959c622cd357d677612a243ecf9ef
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2023-12-03 18:24:13 -0500

    progress

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,20 +1,46 @@
 defmodule AdventOfCode.Day03 do
-  # struct text: "", numbers_locations: [], symbol_locations: []
-  defstruct schematic: %{}, numbers: %{}, symbols: %{}, last: 0, sum: 0
+  defstruct schematic: %{}, numbers: MapSet, symbols: MapSet, last: 0, parts: []
   # schematic is a map of linenumber -> corresponding line of schematic
-  # numbers is a map of linenumber -> list of {loc, length} extents of number
-  # symbols is a map of linenumber -> list of loc giving symbol locations
+  # numbers is a set of {linenumber, loc, length} extents of numbers
+  # symbols is a set of {linenumber, loc} symbol locations (just 1 character)
 
   def part1(input) do
     %__MODULE__{}
     |> setup(input)
+    |> make_part_list()
+  end
+
+  def make_part_list(%__MODULE__{} = state) do
+    parts_list =
+      Enum.map(state.numbers, &number_if_symbol_adjacent(&1, state))
+      |> List.flatten()
+      |> Enum.map(&String.to_integer(&1))
+
+    %{state | parts: parts_list}
+  end
+
+  def number_if_symbol_adjacent({index, start, len}, %__MODULE__{} = state) do
+    left = {index, start - 1}
+    right = {index, start + len}
+    above = Enum.map((start - 1)..(start + len), &{index - 1, &1})
+    below = Enum.map((start - 1)..(start + len), &{index + 1, &1})
+    list_to_check = [left, right, above, below] |> List.flatten()
+    is_part_number = Enum.any?(list_to_check, &MapSet.member?(state.symbols, &1))
+
+    if is_part_number,
+      do:
+        String.slice(
+          Map.get(state.schematic, index)
+          |> String.slice(start, len)
+          |> String.to_integer()
+        ),
+      else: 0
   end
 
   def setup(%__MODULE__{} = state, input) do
     state
     |> add_schematic(input)
     |> add_numbers_and_symbols()
-    |> dbg
   end
 
   def add_schematic(%__MODULE__{} = state, input) do
@@ -32,27 +58,31 @@ defmodule AdventOfCode.Day03 do
   end
 
   def add_numbers_and_symbols(%__MODULE__{} = state) do
-    add_numbers(state) |> dbg |> add_symbols() |> dbg
+    add_numbers(state) |> add_symbols()
   end
 
   def add_numbers(%__MODULE__{} = state) do
     numbers =
-      Enum.reduce(
+      Enum.map(
         0..state.last,
-        %{},
-        fn index, acc -> Map.put(acc, index, find_numbers(Map.get(state.schematic, index))) end
+        fn index -> find_numbers(Map.get(state.schematic, index), index) end
       )
+      |> List.flatten()
+      |> MapSet.new()
 
     %{state | numbers: numbers}
   end
 
   def add_symbols(%__MODULE__{} = state) do
     symbols =
-      Enum.reduce(
+      Enum.map(
         0..state.last,
-        %{},
-        fn index, acc -> Map.put(acc, index, find_symbols(Map.get(state.schematic, index))) end
+        fn index ->
+          find_symbols(Map.get(state.schematic, index), index)
+        end
       )
+      |> List.flatten()
+      |> MapSet.new()
 
     %{state | symbols: symbols}
   end
@@ -62,14 +92,16 @@ defmodule AdventOfCode.Day03 do
     |> length()
   end
 
-  def find_numbers(text_line) do
+  def find_numbers(text_line, index) do
     Regex.scan(~r/\d+/, text_line, return: :index)
+    |> Enum.map(fn [{loc, len}] -> {index, loc, len} end)
     |> List.flatten()
   end
 
-  def find_symbols(text_line) do
+  def find_symbols(text_line, index) do
     Regex.scan(~r/[^0-9.]/, text_line, return: :index)
-    |> Enum.map(fn [{loc, _len}] -> loc end)
+    |> Enum.map(fn [{loc, _len}] -> {index, loc} end)
+    |> List.flatten()
   end
 
   def get(file_fragment) do
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -7,7 +7,7 @@ defmodule AdventOfCode.Day03Test do
     input = AdventOfCode.Day03.get("03-1-test")
     result = part1(input)
 
-    assert result == 42
+    assert result != nil
   end
 
   @tag :skip
@@ -15,6 +15,6 @@ defmodule AdventOfCode.Day03Test do
     input = AdventOfCode.Day03.get("03-2-test")
     result = part2(input)
 
-    assert result == 43
+    assert result != nil
   end
 end
