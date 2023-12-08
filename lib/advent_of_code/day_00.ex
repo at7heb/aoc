a8865ac85b0a2f36c2df91e3b6d968c06fe6f893
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2023-12-07 22:49:08 -0500

    common routines.

---
+++ lib/advent_of_code/day_00.ex
@@ -0,0 +1,8 @@
+defmodule AdventOfCode.Day00 do
+  def get(file_fragment) do
+    # |> dbg
+    file_name = Path.join([".", "games", "day-" <> file_fragment <> ".txt"])
+    # |> dbg
+    File.read!(file_name) |> String.trim()
+  end
+end
