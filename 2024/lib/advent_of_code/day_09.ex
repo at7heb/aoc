f20bcd7d082f0a1a28b02a9c589939418097793d
Howard Bussey <howard.bussey@gmail.com>
2024-12-13 04:40:25 -0500

    wip

--- 2024/lib/advent_of_code/day_09.ex
+++ 2024/lib/advent_of_code/day_09.ex
@@ -1,7 +1,52 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  defstruct input: "",
+  file_map: %{},
+  free_map: %{},
+  current_sector: 0,
+  current_file: 0,
+  current_free: 0,
+  file_count: 0,
+  free_count: 0
+
+
+  def part1(input) do
+    %__MODULE__{}
+    |> parse(input)
+    0
+
   end
 
-  def part2(_args) do
+  def parse(%__MODULE__{} = state, ""), do: state
+  def parse(%__MODULE__{} = state, input) do
+    parse_file(state, input)
+    parse_free(state, input)
+    parse(state, input)
+  end
+
+  def parse_file(%__MODULE__{} = state, ""), do: state
+  def parse_file(%__MODULE__{} = state, input) do
+    {length, sectors, rest} = parse_head_of_input(state, input)
+    new_state = state
+    new_state
+  end
+
+  def parse_free(%__MODULE__{} = state, ""), do: state
+  def parse_free(%__MODULE__{} = state, input) do
+    {length, sectors, rest} = parse_head_of_input(state, input)
+    length = String.to_integer(first)
+    new_state = state
+    new_state
+  end
+
+  def parse_head_of_input(%__MODULE__{} = state, input) do
+    length = String.first(input) |> String.to_integer
+    rest = String.slice(1, 999_999)}
+    sectors = current_sector..current_sector+length-1 |> Enum.to_list()
+    {length, sectors, rest}
+  end
+
+  def part2(input) do
+    # TODO -- fix
+    String.length(input)
   end
 end
