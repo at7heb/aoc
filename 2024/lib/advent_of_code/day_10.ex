819dfc145fc782e2e88ab9910da83fb51ac98993
Howard Bussey <howard.bussey@gmail.com>
2024-12-20 22:01:32 +0100

    part1: wip

--- 2024/lib/advent_of_code/day_10.ex
+++ 2024/lib/advent_of_code/day_10.ex
@@ -1,19 +1,78 @@
 defmodule AdventOfCode.Day10 do
   defstruct input: "",
-            input_copy: ""
+            max_row: 0,
+            max_col: 0,
+            map: %{},
+            trail_heads: [],
+            peaks: [],
+            trails: []
+
+  @day "10"
+
+  def test1(), do: AdventOfCode.Day00.get_test(@day) |> part1()
+  def full1(), do: AdventOfCode.Day00.get(@day) |> part1()
 
   def part1(input) do
     %__MODULE__{}
     |> parse(input)
+    |> find_steps()
     |> info()
     |> find_trail_heads()
+    |> find_peaks()
     |> find_trails()
     |> sum()
     |> dbg
   end
 
+  def find_steps(%__MODULE__{} = state) do
+    new_map =
+    Enum.reduce(get_coords(state), state.map, fn coord, map ->
+      steps = steps_for_location(state, coord)
+      {elevation, _} = Map.get(map, coord)
+      Map.put(map, coord, {elevation, steps})
+    end)
+    %{state| map: new_map}
+  end
+
+  def steps_for_location(%__MODULE__{} = state, {_, _} = coord) do
+    increments = [{0,1}, {1,0}, {0,-1}, {-1, 0}]
+    {elevation, _} = Map.get(state.map, coord)
+    Enum.reduce(increments, [], fn incr, steps_so_far ->
+      if elem(Map.get(state.map, incremented_coord(coord, incr), {-99, nil}), 0) == elevation + 1, do: [incremented_coord(coord, incr)| steps_so_far], else: steps_so_far
+    end)
+
+  end
+
+  def get_coords(%__MODULE__{} = state), do: (for r <- 0..state.max_row, c <- 0..state.max_col do {r, c} end)
+
+  def incremented_coord({r,c}, {ir, ic}), do: {r+ir, c+ic}
+
+  # set a component in state
+  def ss(%__MODULE__{} = state, component, value) when is_atom(component), do: Map.put(state, component, value)
+
   def parse(%__MODULE__{} = state, input) when is_binary(input) do
-    state
+    lines = String.split(input, "\n")
+
+    map =
+      Enum.reduce(0..(length(lines) - 1), %{}, fn line_ndx, map ->
+        setup_line(line_ndx, Enum.at(lines, line_ndx), map)
+      end)
+
+    %{
+      state
+      | map: map,
+        max_row: length(lines) - 1,
+        max_col: String.length(hd(lines)) - 1,
+        input:  lines
+    }
+  end
+
+  def setup_line(line_ndx, line, map) do
+    Enum.reduce(0..(String.length(line) - 1), map, fn col, map ->
+      elevation = String.at(line, col) |> String.to_integer()
+      coord = {line_ndx, col}
+      Map.put(map, coord, {elevation, []})
+    end)
   end
 
   def info(%__MODULE__{} = state) do
@@ -21,11 +80,22 @@ defmodule AdventOfCode.Day10 do
   end
 
   def find_trail_heads(%__MODULE__{} = state) do
-    state
+    trail_heads = Enum.filter(get_coords(state), fn coord -> Map.get(state.map, coord) == 0 end)
+    %{state| trail_heads: trail_heads}
+  end
+  def find_peaks(%__MODULE__{} = state) do
+    peaks = Enum.filter(get_coords(state), fn coord -> Map.get(state.map, coord) == 9 end)
+    %{state| peaks: peaks}
   end
 
   def find_trails(%__MODULE__{} = state) do
-    state
+    trails = Enum.reduce(state.trail_heads, [], fn coord, trails -> trails ++ find_trails_from(state, coord) end)
+    %{state| trails: trails}
+  end
+
+  def find_trails_from(state, coord) do
+    {_, steps} = Map.get(state.map, coord)
+  []
   end
 
   def sum(%__MODULE__{} = state) do
