cb1bd2e93fad0a22335ae69d3c2764df19ff63dc
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2024-12-29 12:34:15 -0500

    add .iex

---
+++ 2024/.iex.exs
@@ -0,0 +1,17 @@
+alias AdventOfCode.Day11
+alias AdventOfCode.Day12
+alias AdventOfCode.Day13
+alias AdventOfCode.Day15
+alias AdventOfCode.Day14
+alias AdventOfCode.Day15
+alias AdventOfCode.Day16
+alias AdventOfCode.Day17
+alias AdventOfCode.Day18
+alias AdventOfCode.Day19
+alias AdventOfCode.Day20
+alias AdventOfCode.Day21
+alias AdventOfCode.Day22
+alias AdventOfCode.Day23
+alias AdventOfCode.Day24
+# .iex.exs
+IEx.configure(inspect: [limit: 10])
