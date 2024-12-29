0e261f67632b277087294958ef525cbb1f024015
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2024-12-29 12:33:43 -0500

    day11 -- wip

--- 2024/lib/advent_of_code/day_11.ex
+++ 2024/lib/advent_of_code/day_11.ex
@@ -1,25 +1,34 @@
 defmodule AdventOfCode.Day11 do
   defstruct stones: [],
-            current_stone: 0
+            current_stone: 0,
+            stone_count: 0
 
   def part1(input) do
     %__MODULE__{}
     |> parse(input)
-    |> dbg
-    |> blink(25)
-    |> answer()
+    |> blink1(25)
+    |> answer1()
+  end
+
+  def part1b(input, times \\ 25) do
+    %__MODULE__{}
+    |> parse(input)
+    |> blink2(times)
+    |> answer2()
   end
 
   def parse(%__MODULE__{} = state, input) do
     stones = input |> String.split(" ", trim: true)
-    %{state | stones: stones}
+    %{state | stones: stones, stone_count: length(stones)}
   end
 
   def transform_all_stones(%__MODULE__{} = state) do
     new_stones =
-      Enum.reduce(state.stones, [], fn stone, new_stones ->
-        new_stones ++ [transform_one_stone(stone)]
+      Enum.reduce(state.stones, [], fn stone, stone_list ->
+        [transform_one_stone(stone) | stone_list]
       end)
+      |> List.flatten()
+      |> Enum.reverse()
 
     %{state | stones: new_stones}
   end
@@ -44,19 +53,63 @@ defmodule AdventOfCode.Day11 do
     [new_stone_1, new_stone_2]
   end
 
-  def blink(%__MODULE__{} = state, number_of_times) do
-    Enum.reduce(1..number_of_times, state.stones, fn _index, stones ->
-      transform_all_stones(stones)
+  def blink1(%__MODULE__{} = state, number_of_times) do
+    Enum.reduce(1..number_of_times, state, fn index, state ->
+      IO.puts("iteration #{index} length = #{length(state.stones)}")
+      transform_all_stones(state)
     end)
   end
 
-  def answer(%__MODULE__{} = state) do
+  def blink2(%__MODULE__{} = state, number_of_times) do
+    cond do
+      length(state.stones) == 0 ->
+        state
+
+      true ->
+        [stone | rest_of_stones] = state.stones
+        new_state = %{state | stones: rest_of_stones}
+        {stone, length(rest_of_stones)} |> dbg
+
+        blink_one_stone(new_state, stone, number_of_times)
+        # |> dbg
+        |> blink2(number_of_times)
+    end
+  end
+
+  def blink_one_stone(%__MODULE__{} = state, _stone, 0), do: state
+
+  def blink_one_stone(%__MODULE__{} = state, stone, number_of_times) when is_binary(stone) do
+    {stone, number_of_times, state.stones, state.stone_count} |> dbg
+    new = transform_one_stone(stone)
+
+    {state, new_stone} =
+      cond do
+        length(new) == 2 -> {push(state, new |> tl() |> hd()), hd(new)}
+        true -> {state, hd(new)}
+      end
+
+    blink_one_stone(state, new_stone, number_of_times - 1)
+  end
+
+  def push(%__MODULE__{} = state, stone) when is_binary(stone) do
+    new_stones = [stone | state.stones]
+    {stone, new_stones} |> dbg
+    if state.stone_count > 50, do: raise(ArgumentError, message: "")
+    %{state | stones: new_stones, stone_count: state.stone_count + 1}
+  end
+
+  def answer1(%__MODULE__{} = state) do
     length(state.stones)
   end
+
+  def answer2(%__MODULE__{} = state), do: state.stone_count
 
   def part2(input) do
     %__MODULE__{}
     |> parse(input)
+    |> dbg
+    |> blink2(75)
+    |> answer2()
   end
 
   def test1(), do: part1("125 17")
